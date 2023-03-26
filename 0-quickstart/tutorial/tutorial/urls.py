8f6cf5d2941a4e64de5880161c1bee47555115ad
mishrayushi2001 <mishrayushi2000@gmail.com>
2023-03-27 00:27:22 +0530

    quickstart

--- 0-quickstart/tutorial/tutorial/urls.py
+++ 0-quickstart/tutorial/tutorial/urls.py
@@ -27,7 +27,7 @@ router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+# Additionally, we i nclude login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
